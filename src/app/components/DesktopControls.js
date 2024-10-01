89956f771c8e6fb626ed6da81fb5b242aaa8dbce
Jack Robetson <jackrobertsonps3@gmail.com>
2024-10-01 17:33:08 +0100

    Updates to ui and Desktop controles

--- src/app/components/DesktopControls.js
+++ src/app/components/DesktopControls.js
@@ -1,229 +1,63 @@
-import React, {useState, useEffect, useRef} from "react";
-import {Button, Typography, Box, IconButton} from "@mui/material";
+import React from "react";
+import {Typography, Box} from "@mui/material";
 import {styled} from "@mui/system";
-import Slider from "react-slick";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import LocalBarIcon from "@mui/icons-material/LocalBar";
-import MoneyOffIcon from "@mui/icons-material/MoneyOff";
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import Flag from "react-world-flags"; // Import the Flag component
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
+// Styled components
 const BackgroundContainer = styled(Box)({
     position: "fixed",
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: "#bdc3c790", // Black background
-    zIndex: 999, // Below the widget container
-});
-
-const DesktopContainer = styled(Box)(({isFading}) => ({
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "80%",
-    height: "80%",
+    backgroundColor: "#fab613", // Solid yellow background
+    zIndex: 999,
     display: "flex",
-    flexDirection: "column",
     justifyContent: "space-between",
     alignItems: "center",
-    backgroundColor: "#f7fbfc",
-    color: "#000",
-    padding: "20px",
-    zIndex: 1000,
-    transition: "opacity 0.5s ease-out",
-    opacity: isFading ? 0 : 1,
-    pointerEvents: isFading ? "none" : "auto",
+    padding: "0 40px", // Add padding for some space
     boxSizing: "border-box",
-}));
+});
 
-const TitleBox = styled(Box)({
+const MessageContainer = styled(Box)({
+    width: "50%", // Adjust to fit the message and illustration layout
     textAlign: "left",
-    width: "100%",
-    padding: "30px",
-    boxSizing: "border-box",
-    flex: 1,
-    overflowY: "auto",
-});
-
-const FullWidthButton = styled(Button)({
-    backgroundColor: "#fab613",
-    color: "#000",
-    width: "calc(100%)",
     fontWeight: "bold",
-    "&:hover": {
-        backgroundColor: "#d35400",
-    },
-    padding: "15px 25px",
-    marginBottom: "30px",
-    boxSizing: "border-box",
-    alignSelf: "center",
 });
 
-const ProgressContainer = styled(Box)({
+const IllustrationContainer = styled(Box)({
+    width: "40%", // Set width of the SVG illustration container
     display: "flex",
-    width: "100%",
-    padding: "0 10px",
-    boxSizing: "border-box",
-    justifyContent: "space-between",
-    marginBottom: "20px",
+    justifyContent: "flex-end", // Align the SVG to the right
 });
 
-const ProgressBarSection = styled(Box)(({completed}) => ({
-    flex: 1,
-    height: "5px",
-    margin: "0 2px",
-    backgroundColor: "#ddd",
-    borderRadius: "2px",
-    position: "relative",
-    "&::after": {
-        content: '""',
-        display: "block",
-        height: "100%",
-        width: `${completed}%`,
-        backgroundColor: "#fab613",
-        borderRadius: "2px",
-        transition: "width 0.5s ease-out",
-    },
-}));
-
-const ArrowButton = styled(IconButton)({
-    position: "absolute",
-    top: "50%",
-    transform: "translateY(-50%)",
-    zIndex: 1001, // Above the background, below the container
-    backgroundColor: "#fab613",
-    color: "#ffffff", // Orange color for the arrows
-    "&:hover": {
-        backgroundColor: "#d35400",
-    },
-});
-
-const PrevArrow = ({onClick}) => (
-    <ArrowButton style={{left: "10px"}} onClick={onClick}>
-        <ArrowBackIosIcon />
-    </ArrowButton>
-);
-
-const NextArrow = ({onClick}) => (
-    <ArrowButton style={{right: "10px"}} onClick={onClick}>
-        <ArrowForwardIosIcon />
-    </ArrowButton>
-);
-
-const DesktopControls = ({fetchPubs}) => {
-    const [isFading, setIsFading] = useState(false);
-    const [isVisible, setIsVisible] = useState(true);
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const [progress, setProgress] = useState([0, 0, 0, 0]);
-    const sliderRef = useRef(null);
-
-    useEffect(() => {
-        const updatedProgress = progress.map((value, index) => (index <= currentSlide ? 100 : 0));
-        setProgress(updatedProgress);
-    }, [currentSlide]);
-
-    const handleButtonClick = () => {
-        setIsFading(true);
-        setTimeout(() => {
-            fetchPubs();
-            setIsVisible(false);
-        }, 500);
-    };
-
-    const handleSlideChange = (index) => {
-        setCurrentSlide(index);
-    };
-
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        beforeChange: (oldIndex, newIndex) => handleSlideChange(newIndex),
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-    };
-
-    if (!isVisible) return null;
-
+const DesktopControls = () => {
     return (
-        <>
-            <BackgroundContainer />
-            <DesktopContainer isFading={isFading}>
-                <ProgressContainer>
-                    {progress.map((completed, index) => (
-                        <ProgressBarSection key={index} completed={completed} />
-                    ))}
-                </ProgressContainer>
-                <Slider {...settings} ref={sliderRef} style={{width: "100%", height: "100%", marginBottom:'2em'}}>
-                    <div>
-                        <TitleBox>
-                            <Typography variant="h3" gutterBottom>
-                                Welcome to <b style={{color: "#fab613"}}>PINTS!</b>
-                            </Typography>
-                            <Typography
-                                variant="body1"
-                                style={{
-                                    fontWeight: 400,
-                                    lineHeight: "1.8",
-                                }}
-                                paragraph
-                            >
-                                <b>PINTS</b> is your trusty companion for finding the best pub deals around you! If you're
-                                in Central London, simply share your location, and <b>PINTS</b> will pinpoint the three
-                                cheapest pints in your area. Whether you're exploring a new part of town or enjoying a night
-                                out in your favorite area, <b>PINTS</b> ensures you always get the best value for your pint.
-                                Discover great pubs and save money with <b>PINTS</b> – cheers to that!
-                            </Typography>
-                        </TitleBox>
-                    </div>
-                    <div>
-                        <TitleBox>
-                            <LocationOnIcon style={{fontSize: 60, color: "#fab613"}} />
-                            <Typography variant="h3" gutterBottom>
-                                Share Your Location
-                            </Typography>
-                            <Typography variant="body1" paragraph>
-                                Enable location services so we can find the best pub deals near you.
-                            </Typography>
-                        </TitleBox>
-                    </div>
-                    <div>
-                        <TitleBox>
-                            <LocalBarIcon style={{fontSize: 60, color: "#fab613"}} />
-                            <Typography variant="h3" gutterBottom>
-                                Find Pubs
-                            </Typography>
-                            <Typography variant="body1" paragraph>
-                                Discover great pubs around you with the best deals on drinks.
-                            </Typography>
-                        </TitleBox>
-                    </div>
-                    <div>
-                        <TitleBox>
-                            <MoneyOffIcon style={{fontSize: 60, color: "#fab613"}} />
-                            <Typography variant="h3" gutterBottom>
-                                Save Money
-                            </Typography>
-                            <Typography variant="body1" paragraph>
-                                Enjoy your time out while saving money on drinks in the process.
-                            </Typography>
-                            <FullWidthButton variant="contained" onClick={handleButtonClick}>
-                                Get me to the pub!
-                            </FullWidthButton>
-                        </TitleBox>
-                    </div>
-                </Slider>
-            </DesktopContainer>
-        </>
+        <BackgroundContainer>
+            {/* Image (SVG) */}
+            <IllustrationContainer>
+                <img
+                    src="/Launch_Images/Welcome.svg"
+                    alt="Illustration"
+                    style={{maxWidth: "80%", height: "auto"}} // Set the desired width
+                />
+            </IllustrationContainer>
+            {/* Message Box */}
+            <MessageContainer>
+                <Typography
+                    variant="h2"
+                    gutterBottom
+                    style={{fontWeight: "bold", fontSize: "5rem", color: "#000333", lineHeight:'.9'}} // Adjust the font size and color
+                >
+                    Oops, looks like you're trying to access us on a desktop!
+                </Typography>
+                <Typography
+                    variant="h5"
+                    style={{fontSize: "2rem", color: "#000333"}} // Slightly bigger and apply the same color
+                >
+                    We’re all about that mobile life. Grab your phone and come hang with us there. 😎📱
+                </Typography>
+            </MessageContainer>
+        </BackgroundContainer>
     );
 };
 
--- src/app/components/map/SlideUpPanel.js
+++ src/app/components/map/SlideUpPanel.js
@@ -8,6 +8,8 @@ import {firestore} from "../../../../config/firebaseConfig"; // Adjust the impor
 import {getAuth} from "firebase/auth";
 import {CSSTransition} from "react-transition-group";
 import {TransitionGroup} from "react-transition-group";
+import SportsBarIcon from "@mui/icons-material/SportsBar";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import "../../styles/animations.css"; // Create a CSS file for the animations
 
 const PanelContainer = styled(Box)(({isVisible}) => ({
@@ -17,7 +19,7 @@ const PanelContainer = styled(Box)(({isVisible}) => ({
     right: 0,
     backgroundColor: "#f7fbfc",
     color: "#000",
-    boxShadow: "0 -2px 10px rgba(0, 0, 0, 0.3)",
+    boxShadow: "0 -2px 10px rgba(0, 0, 0, 0.1)",
     transform: `translateY(${isVisible ? "0" : "100%"})`,
     transition: "transform 0.3s ease",
     padding: "20px",
@@ -100,16 +102,32 @@ const SlideUpPanel = ({pub, isVisible, onClose}) => {
     const auth = getAuth();
     const user = auth.currentUser;
 
-    useEffect(() => {
-        if (user && pub) {
-            logPubClick(pub.id, user.uid);
-        }
-    }, [pub, user]);
+    // Set this to true when working offline
+    const isOffline = true;
 
-    if (!pub) return null;
+    // Mock pub data for offline mode
+    const mockPub = {
+        id: "mockPubId",
+        pub_name: "Mock Pub Name",
+        pint_type: "Mock Pint Type",
+        pint_price: "£5.00",
+        latitude: 51.5074,
+        longitude: -0.1276,
+    };
+
+    // Use mock pub data when offline
+    const currentPub = isOffline ? mockPub : pub;
+
+    useEffect(() => {
+        if (user && currentPub && !isOffline) {
+            logPubClick(currentPub.id, user.uid);
+        }
+    }, [currentPub, user, isOffline]);
+
+    if (!currentPub) return null;
 
     const handleTakeMeButtonClick = () => {
-        const coords = `${pub.latitude},${pub.longitude}`;
+        const coords = `${currentPub.latitude},${currentPub.longitude}`;
         navigator.clipboard
         .writeText(coords)
         .then(() => {
@@ -132,7 +150,7 @@ const SlideUpPanel = ({pub, isVisible, onClose}) => {
         }
 
         try {
-            const pubDoc = doc(firestore, "pubs", pub.id);
+            const pubDoc = doc(firestore, "pubs", currentPub.id);
             await updateDoc(pubDoc, updatedData);
             toast.success("Pub information updated successfully!");
             setEditMode(false);
@@ -168,27 +186,49 @@ const SlideUpPanel = ({pub, isVisible, onClose}) => {
                         <IconButton onClick={onClose} style={{position: "absolute", top: "10px", right: "10px"}}>
                             <CloseIcon style={{color: "#fab613"}} />
                         </IconButton>
-                        <Typography
-                            variant="h4"
-                            gutterBottom
+
+                        {/* Pint Price */}
+                        <div
                             style={{
-                                textAlign: "center",
+                                textAlign: "left",
                                 marginTop: "0em",
                                 marginBottom: "0em",
                                 fontSize: "5em",
                                 fontWeight: "bold",
+                                letterSpacing: "-0.01em",
+                                lineHeight: "1", // Add custom line-height
                             }}
                         >
-                            {pub.pint_price}
+                            {currentPub.pint_price}
+
+                            <div gutterBottom style={{marginTop: "0.5em", textAlign: "left", fontSize: ".3em"}}>
+                                {currentPub.pub_name}
+                            </div>
+                        </div>
+
+                        {/* Pub Address */}
+                        <Box display="flex" alignItems="center" gap="5px" justifyContent="left">
+                        <Typography variant="body1" style={{textAlign: "left", marginBottom: "0em", color: "gray", marginLeft: "0"}}>
+                            <LocationOnIcon style={{ fontSize: "30px", verticalAlign: "middle", marginLeft: "0", }} />{" "}
+                            {currentPub.pub_address || "Mock Pub Address, Central London"}{" "}
+                            {/* Mock data or real pub address */}
                         </Typography>
-                        <Typography variant="h6" gutterBottom style={{marginTop: "1em", textAlign: "center"}}>
-                            {pub.pub_name}
-                        </Typography>
-                        <Box display="flex" alignItems="center" gap="10px" justifyContent="center">
-                            <Typography variant="subtitle1" paragraph>
-                                <strong style={{color: "#fab613"}}>Pint Type:</strong> {pub.pint_type}
+                        </Box>
+
+                        <hr style={{borderTop: "1px solid lightgray", margin: ".2em 0"}} />
+
+                        {/* Beer Icon and Kind of Pint */}
+                        <Box display="flex" alignItems="center" gap="5px" justifyContent="left">
+                            <SportsBarIcon style={{color: "#fab613", fontSize: "30px"}} />
+                            <Typography variant="subtitle1" style={{fontWeight: "bold", color: "#fab613"}}>
+                                {currentPub.pint_type}
                             </Typography>
                         </Box>
+
+                        {/* Divider Line */}
+                        <hr style={{borderTop: "1px solid lightgray", margin: ".2em 0"}} />
+
+                        {/* Buttons */}
                         <StyledButton
                             variant="contained"
                             onClick={handleTakeMeButtonClick}
@@ -212,19 +252,19 @@ const SlideUpPanel = ({pub, isVisible, onClose}) => {
                     <PanelContainer isVisible={isVisible}>
                         <StyledTextField
                             label="Pub Name"
-                            defaultValue={pub.pub_name}
+                            defaultValue={currentPub.pub_name}
                             onChange={(e) => handleInputChange("pub_name", e.target.value)}
                             fullWidth
                         />
                         <StyledTextField
                             label="Pint Type"
-                            defaultValue={pub.pint_type}
+                            defaultValue={currentPub.pint_type}
                             onChange={(e) => handleInputChange("pint_type", e.target.value)}
                             fullWidth
                         />
                         <StyledTextField
                             label="Pint Price"
-                            defaultValue={pub.pint_price}
+                            defaultValue={currentPub.pint_price}
                             onChange={(e) => handleInputChange("pint_price", e.target.value)}
                             fullWidth
                         />
